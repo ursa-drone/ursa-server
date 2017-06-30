980555dccb498a72ce534f02323007e8ca17ca2f
lachyd <lachlan.d@live.com.au>
2017-06-30 10:50:43 +1000

    Include simulation capabilities

--- src/ursa/config/ursa_sim.lua
+++ src/ursa/config/ursa_sim.lua
@@ -1,21 +1,11 @@
--- Copyright 2016 The Cartographer Authors
---
--- Licensed under the Apache License, Version 2.0 (the "License");
--- you may not use this file except in compliance with the License.
--- You may obtain a copy of the License at
---
---      http://www.apache.org/licenses/LICENSE-2.0
---
--- Unless required by applicable law or agreed to in writing, software
--- distributed under the License is distributed on an "AS IS" BASIS,
--- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
--- See the License for the specific language governing permissions and
--- limitations under the License.
+-- Copyright 2017 LAOSAAC
 
 include "map_builder.lua"
+include "trajectory_builder.lua"
 
 options = {
   map_builder = MAP_BUILDER,
+  trajectory_builder = TRAJECTORY_BUILDER,
   map_frame = "map",
   tracking_frame = "base_link",
   published_frame = "base_link",
@@ -28,11 +18,18 @@ options = {
   lookup_transform_timeout_sec = 0.2,
   submap_publish_period_sec = 0.3,
   pose_publish_period_sec = 5e-3,
+  trajectory_publish_period_sec = 30e-3,
 }
 
+-- Use 2D map builder
 MAP_BUILDER.use_trajectory_builder_2d = true
-TRAJECTORY_BUILDER_2D.laser_max_range = 50.
-TRAJECTORY_BUILDER_2D.laser_missing_echo_ray_length = 5.
+
+-- Custom parameters
+TRAJECTORY_BUILDER_2D.use_imu_data = false
+TRAJECTORY_BUILDER_2D.max_range = 50.
 TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
+TRAJECTORY_BUILDER_2D.missing_data_ray_length = 1
+TRAJECTORY_BUILDER_2D.submaps.range_data_inserter.miss_probability = 0.49
+
 
 return options
