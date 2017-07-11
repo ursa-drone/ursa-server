58c39a57be2398db63f6f34bf32dc011082ce544
Isaac <IsaacHook@gmail.com>
2017-07-11 15:54:05 +1000

    updated config files for rviz & cartographer

--- src/ursa/config/ursa_sim.lua
+++ src/ursa/config/ursa_sim.lua
@@ -1,35 +1,9 @@
 -- Copyright 2017 LAOSAAC
 
-include "map_builder.lua"
-include "trajectory_builder.lua"
+include "cartographer_config.lua"
 
-options = {
-  map_builder = MAP_BUILDER,
-  trajectory_builder = TRAJECTORY_BUILDER,
-  map_frame = "map",
-  tracking_frame = "base_link",
-  published_frame = "base_link",
-  odom_frame = "odom",
-  provide_odom_frame = true,
-  use_odometry = false,
-  use_laser_scan = true,
-  use_multi_echo_laser_scan = false,
-  num_point_clouds = 0,
-  lookup_transform_timeout_sec = 0.2,
-  submap_publish_period_sec = 0.3,
-  pose_publish_period_sec = 5e-3,
-  trajectory_publish_period_sec = 30e-3,
-}
-
--- Use 2D map builder
-MAP_BUILDER.use_trajectory_builder_2d = true
-
--- Custom parameters
+-- Custom simulation parameters
 TRAJECTORY_BUILDER_2D.use_imu_data = false
 TRAJECTORY_BUILDER_2D.max_range = 50.
-TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
-TRAJECTORY_BUILDER_2D.missing_data_ray_length = 1
-TRAJECTORY_BUILDER_2D.submaps.range_data_inserter.miss_probability = 0.49
-
 
 return options
